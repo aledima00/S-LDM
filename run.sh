90dbecfb7c6962747351f8c59a0fbbb509d47813
Alessandro Di Matteo <aledimatteo00@gmail.com>
2026-03-02 03:03:48 +0100

    updated run script and added utility scripts

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 TZ="Europe/Rome"
-SLDM_INTERNAL_AREA="45.10:7.69-45.17:7.82"
-SLDM_EXTERNAL_AREA_LAT_FACTOR="0.0002"
-SLDM_EXTERNAL_AREA_LON_FACTOR="0.0002"
+#SLDM_INTERNAL_AREA="45.064973:7.674826-45.065027:7.681174"
+SLDM_INTERNAL_AREA="45.063:7.673-45.066:7.683"
+SLDM_EXTERNAL_AREA_LAT_FACTOR="0.000001"
+SLDM_EXTERNAL_AREA_LON_FACTOR="0.000001"
 BROKER_URL="127.0.0.1:5672"
 AMQP_TOPIC="topic://5gcarmen.examples"
 MS_REST_ADDRESS=http://localhost
@@ -15,5 +16,7 @@ GNN_STEP_LEN_MS=100
 GNN_PACK_SIZE=100
 GNN_STRIDE=1
 GNN_TRIGGERING_THRESHOLD=0.5
+GNN_NETOFFSET="250.00,-6.24"
+DISABLE_GNN_TRIGGER="--disable-gnn-trigger"
 
-bash -c "./SLDM --disable-misbehaviour-detector --disable-quadkey-filter --gnn-snapshot-path ${GNN_SNAPSHOT_PATH} --gnn-step-len ${GNN_STEP_LEN_MS} --gnn-pack-size ${GNN_PACK_SIZE} --gnn-stride ${GNN_STRIDE} --gnn-triggering-threshold ${GNN_TRIGGERING_THRESHOLD} -A ${SLDM_INTERNAL_AREA} --ext-area-lat-factor ${SLDM_EXTERNAL_AREA_LAT_FACTOR} --ext-area-lon-factor ${SLDM_EXTERNAL_AREA_LON_FACTOR} --broker-url ${BROKER_URL} --broker-queue ${AMQP_TOPIC} --ms-rest-address ${MS_REST_ADDRESS} --ms-rest-port ${MS_REST_PORT} --vehviz-nodejs-address ${VEHVIZ_UDP_ADDRESS} --vehviz-nodejs-port ${VEHVIZ_UDP_PORT} --vehviz-web-port ${VEHVIZ_WEB_PORT} -L ${LOG_FILE}"
+bash -c "./SLDM --disable-misbehaviour-detector --disable-quadkey-filter --gnn-snapshot-path ${GNN_SNAPSHOT_PATH} --gnn-step-len ${GNN_STEP_LEN_MS} --gnn-pack-size ${GNN_PACK_SIZE} --gnn-stride ${GNN_STRIDE} --gnn-triggering-threshold ${GNN_TRIGGERING_THRESHOLD} --gnn-sumo-netoffset ${GNN_NETOFFSET} -A ${SLDM_INTERNAL_AREA} --ext-area-lat-factor ${SLDM_EXTERNAL_AREA_LAT_FACTOR} --ext-area-lon-factor ${SLDM_EXTERNAL_AREA_LON_FACTOR} --broker-url ${BROKER_URL} --broker-queue ${AMQP_TOPIC} --ms-rest-address ${MS_REST_ADDRESS} --ms-rest-port ${MS_REST_PORT} --vehviz-nodejs-address ${VEHVIZ_UDP_ADDRESS} --vehviz-nodejs-port ${VEHVIZ_UDP_PORT} --vehviz-web-port ${VEHVIZ_WEB_PORT} -L ${LOG_FILE}"
