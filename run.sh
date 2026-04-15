2f04e95284e8db74a361126cec249137b050752f
Alessandro Di Matteo <aledimatteo00@gmail.com>
2026-04-15 19:04:31 +0200

    updated docker structure, gitignore annd removed unused, updated runner

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 TZ="Europe/Rome"
-SLDM_INTERNAL_AREA="45.059:7.670-45.071:7.685"
+SLDM_INTERNAL_AREA="45.059394:7.671071-45.070606:7.684931"
 SLDM_EXTERNAL_AREA_LAT_FACTOR="0.000001"
 SLDM_EXTERNAL_AREA_LON_FACTOR="0.000001"
 BROKER_URL="127.0.0.1:5672"
@@ -11,13 +11,13 @@ VEHVIZ_UDP_PORT=48110
 VEHVIZ_WEB_PORT=8080
 VEHVIZ_UPDATE_INTERVAL_S=0.1
 #LOG_FILE="logs/log.txt"
-GNN_SNAPSHOT_PATH="gnn/input/lb3.2seq.pth"
-GNN_CSV_OUT_PATH="out/prediction.csv"
+GNN_SNAPSHOT_PATH="gnn/input/lb3.3.pth"
+GNN_CSV_OUT_PATH="out/p3.3.csv"
 GNN_STEP_LEN_MS=100
 GNN_PACK_SIZE=80
-GNN_STRIDE=5
+GNN_STRIDE=1
 GNN_TRIGGERING_THRESHOLD=0.5
-GNN_NETOFFSET="250.00,-6.24"
+GNN_NETOFFSET="545.78,622.98"
 DISABLE_GNN_TRIGGER="--disable-gnn-trigger"
 
 bash -c "./SLDM --disable-misbehaviour-detector --disable-quadkey-filter --gnn-snapshot-path ${GNN_SNAPSHOT_PATH} --gnn-csv-out-path ${GNN_CSV_OUT_PATH} --gnn-step-len ${GNN_STEP_LEN_MS} --gnn-pack-size ${GNN_PACK_SIZE} --gnn-stride ${GNN_STRIDE} --gnn-triggering-threshold ${GNN_TRIGGERING_THRESHOLD} --gnn-sumo-netoffset ${GNN_NETOFFSET} -A ${SLDM_INTERNAL_AREA} --ext-area-lat-factor ${SLDM_EXTERNAL_AREA_LAT_FACTOR} --ext-area-lon-factor ${SLDM_EXTERNAL_AREA_LON_FACTOR} --broker-url ${BROKER_URL} --broker-queue ${AMQP_TOPIC} --ms-rest-address ${MS_REST_ADDRESS} --ms-rest-port ${MS_REST_PORT} --vehviz-nodejs-address ${VEHVIZ_UDP_ADDRESS} --vehviz-nodejs-port ${VEHVIZ_UDP_PORT} --vehviz-web-port ${VEHVIZ_WEB_PORT} --vehviz-update-interval ${VEHVIZ_UPDATE_INTERVAL_S}"
